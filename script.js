2457a12c9b5d95212647faf6a733d877dda47e6f
Uzair-Arif96 <mohammad.uzairarif07@gmail.com>
2025-08-08 21:38:47 +0600

    Added Website Files

---
+++ script.js
@@ -0,0 +1,12 @@
+function generateName() {
+    const city = document.getElementById("city").value.trim();
+    const pet = document.getElementById("pet").value.trim();
+
+    if (city && pet) {
+        document.getElementById("result").textContent =
+            `Your band name could be: ${city} ${pet}'s!`;
+    } else {
+        document.getElementById("result").textContent =
+            "Please fill in both fields!";
+    }
+}
